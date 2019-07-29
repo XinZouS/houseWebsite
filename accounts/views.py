ab964207ac58d8b8d75f09e1188f6f0eaf719644
XinZouS <sanking.saphira@foxmail.com>
2019-07-29 17:21:40 -0400

    add register page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def register(request):
+	if request.method == 'POST':
+		# get vals
+		first_name = request.POST['first_name']
+		last_name = request.POST['last_name']
+		username = request.POST['username']
+		email = request.POST['email']
+		password1 = request.POST['password']
+		password2 = request.POST['password2']
+
+		testOk = True
+		if password1 != password2:
+			messages.error(request, 'Passwords do not match')
+			request.GET['password'] = ''
+			request.GET['password2'] = ''
+			testOk = False
+
+		if User.objects.filter(username=username).exists():
+			messages.error(request, 'The username is taken')
+			testOk = False
+
+		if User.objects.filter(email=email).exists():
+			messages.error(request, 'The email is being used')
+			testOk = False
+
+		if not testOk:
+			values = {
+				'first_name': first_name,
+				'last_name': last_name,
+				'username': username,
+				'email': email,
+			}
+			context = {
+				'values': values,
+			}
+			return render(request, 'accounts/register.html', context)
+
+		# looks good, create user
+		user = User.objects.create_user(username=username, password=password1,
+			email=email, first_name=first_name, last_name=last_name)
+		# login after reigster
+		# auth.login(request, user)
+		# messages.success(request, 'You are now logged in')
+		# return redirect('index')
+		user.save()
+		messages.success(request, 'You are registered')
+		return redirect('login')
+
+	return render(request, 'accounts/register.html')
+
+def login(request):
+	if request.method == 'POST':
+		# login user
+		return
+
+	return render(request, 'accounts/login.html')
+
+def logout(request):
+	return render(request, 'accounts/logout.html')
+
+def dashboard(request):
+	return render(request, 'accounts/dashboard.html')
+
--- btre/settings.py
+++ btre/settings.py
@@ -38,10 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
-    'django.contrib.humanize',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -140,4 +141,14 @@ STATICFILES_DIRS = [
 # media folder setting
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+# Messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.INFO: 'info',
+    messages.ERROR: 'danger',
+}
+
+
 
